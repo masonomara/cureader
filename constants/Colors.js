e2473fffe29ae075be76bdc743adf53c77a9d435
masonomara <mason.omara@gmail.com>
2023-10-21 19:36:04 -0400

    tried to disbale dark theme

--- constants/Colors.js
+++ constants/Colors.js
@@ -10,10 +10,19 @@ export default {
     tabIconSelected: tintColorLight,
   },
   dark: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  /*
+  dark: {
     text: '#fff',
     background: '#000',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
   },
+  */
 };
