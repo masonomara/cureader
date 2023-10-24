5cbd26947dc659242a744def22c2685a2398ffcb
masonomara <mason.omara@gmail.com>
2023-10-24 04:13:08 -0400

    changed (login)/index to login

--- app/(home)/_layout.jsx
+++ app/(home)/_layout.jsx
@@ -21,7 +21,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="login"
         options={{
           headerStyle: {
             shadowColor: 'transparent', // Remove shadow on iOS
