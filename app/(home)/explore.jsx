69063aba69f4e646c6d17b7ec8cae62c6b7b3f2b
masonomara <mason.omara@gmail.com>
2023-11-03 00:31:44 -0400

    started cleaning up explore page code

--- app/(home)/explore.jsx
+++ app/(home)/explore.jsx
@@ -16,8 +16,6 @@ import Colors from "../../constants/Colors";
 
 export default function TabOneScreen() {
   const colorScheme = useColorScheme();
-  const [rssChannels, setRssChannels] = useState([]);
-  const [rssItems, setRssItems] = useState([]);
   const [user, setUser] = useState(null);
 
   // redirect based on if user exists
@@ -29,63 +27,7 @@ export default function TabOneScreen() {
     });
   }, []);
 
-  const doLogout = async () => {
-    const { error } = await supabase.auth.signOut();
-    router.replace("(login)");
-    if (error) {
-      Alert.alert("Error Signing Out User", error.message);
-    }
-  };
 
-  // parse feeds
-  useEffect(() => {
-    const feedUrls = [
-      "https://feeds.megaphone.fm/newheights",
-      "http://www.nasa.gov/rss/dyn/breaking_news.rss",
-      "https://podcastfeeds.nbcnews.com/RPWEjhKq",
-      // Add more RSS feed URLs here
-    ];
-
-    const fetchAndParseFeeds = async () => {
-      const allChannels = [];
-      const allItems = [];
-
-      const parseAndSort = async (url) => {
-        try {
-          const response = await fetch(url);
-          const responseData = await response.text();
-          const parsedRss = await rssParser.parse(responseData);
-
-          allChannels.push({
-            title: parsedRss.title,
-            description: parsedRss.description,
-          });
-
-          allItems.push(
-            ...parsedRss.items.map((item) => ({
-              ...item,
-              publicationDate: new Date(item.published),
-              channel: parsedRss.title, // Include the channel title in the item
-              image: parsedRss.image,
-              channelUrl: parsedRss.links[0].url,
-            }))
-          );
-        } catch (error) {
-          console.error(error);
-        }
-      };
-
-      await Promise.all(feedUrls.map(parseAndSort));
-
-      // Sort items by publication date in descending order (most recent first)
-      allItems.sort((a, b) => b.publicationDate - a.publicationDate);
-
-      setRssChannels(allChannels);
-      setRssItems(allItems);
-    };
-
-    fetchAndParseFeeds();
-  }, []);
 
   const styles = {
     container: {
@@ -128,34 +70,13 @@ export default function TabOneScreen() {
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text numberOfLines={20}>{JSON.stringify(user, null, 2)}</Text>
-        <TouchableOpacity onPress={doLogout}>
-          <Text>Log out</Text>
-        </TouchableOpacity>
-      </View>
-      <TextInput
-        style={styles.input}
-        label="Email"
-        // onChangeText={(text) => setEmail(text)}
-        // value={email}
-        placeholder="email"
-        autoCapitalize={"none"}
-        autoCorrect={false}
-      />
       <FlatList
         data={rssItems}
         keyExtractor={(item, index) => index.toString()}
         style={styles.articleList}
         renderItem={({ item }) => {
           return (
-            <ArticleCard
-              item={item}
-              publication={item.channel}
-              image={item.image}
-              channelUrl={item.channelUrl}
-              user={user}
-            />
+            <Text>{item.title}</Text>
           );
         }}
       />
