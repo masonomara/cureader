7d42e621961b348f698e50d1c8b1f66006aef8c3
masonomara <mason.omara@gmail.com>
2023-11-04 08:21:41 -0400

    moved "channels" to "explore"

--- app/(home)/explore.jsx
+++ app/(home)/explore.jsx
@@ -1,46 +1,56 @@
-import React, { useState, useEffect } from "react";
-import {
-  FlatList,
-  TouchableOpacity,
-  Alert,
-  useColorScheme,
-  TextInput,
-} from "react-native";
-import { router } from "expo-router";
+import { ScrollView, useColorScheme, Text, TextInput } from "react-native";
+import { View } from "../../components/Themed";
+import { useState, useEffect } from "react";
+import ChannelCardFeatured from "../../components/ChannelCardFeatured";
+import ChannelCard from "../../components/ChannelCard";
 import { supabase } from "../../config/initSupabase";
-import { Text, View } from "../../components/Themed";
-import * as rssParser from "react-native-rss-parser";
-import ArticleCard from "../../components/ArticleCard";
-import { Input } from "react-native-elements";
 import Colors from "../../constants/Colors";
 
 export default function TabOneScreen() {
   const colorScheme = useColorScheme();
-  const [user, setUser] = useState(null);
+  const [popularChannels, setPopularChannels] = useState([]);
 
-  // redirect based on if user exists
-  useEffect(() => {
-    supabase.auth.getUser().then(({ data: { user } }) => {
-      if (user) {
-        setUser(user);
+  // Function to fetch all entries in the "channels" table
+  const fetchChannels = async () => {
+    try {
+      const { data, error } = await supabase
+        .from("channels")
+        .select("*")
+        .order("channel_subscribers", { ascending: false }); // Sort by the "channel_subscribers" column in descending order
+
+      if (error) {
+        console.error("Error fetching channels:", error);
+        return []; // Return an empty array in case of an error
+      } else {
+        console.log("List of channels sorted by subscribers:", data);
+        return data; // Return the data if fetched successfully
       }
-    });
+    } catch (error) {
+      console.error("Error fetching channels:", error);
+      return []; // Return an empty array in case of an error
+    }
+  };
+
+  useEffect(() => {
+    // Fetch channels when the component mounts
+    const fetchData = async () => {
+      const channelsData = await fetchChannels();
+      setPopularChannels(channelsData); // Set the state with the fetched data
+    };
+
+    fetchData();
   }, []);
 
   const styles = {
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    articleList: {
-      width: "100%",
+    container: {},
+    channelList: {
+      gap: 12,
+      paddingHorizontal: 16,
     },
     input: {
-      width: "100%",
+      flex: 1,
       borderRadius: 20,
       height: 56,
-      marginBottom: 16,
       paddingHorizontal: 16,
       borderWidth: 1,
       flexDirection: "row",
@@ -67,19 +77,34 @@ export default function TabOneScreen() {
   };
 
   return (
-    <View style={styles.container}>
-      {/*
-      <FlatList
-        data={rssItems}
-        keyExtractor={(item, index) => index.toString()}
-        style={styles.articleList}
-        renderItem={({ item }) => {
-          return (
-            <Text>{item.title}</Text>
-          );
-        }}
+    <ScrollView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        label="Channel Url Text"
+        // onChangeText={handleUserInput}
+        // value={userInput}
+        placeholder="Search for channel placeholder"
+        autoCapitalize={"none"}
+        autoCorrect={false}
       />
-      */}
-    </View>
+      {popularChannels && popularChannels.length > 0 ? (
+        <ScrollView horizontal contentContainerStyle={styles.channelList}>
+          {popularChannels.map((item) => (
+            <ChannelCardFeatured key={item.id} item={item} />
+          ))}
+        </ScrollView>
+      ) : (
+        <Text>Loading...</Text> // You can display a loading message here
+      )}
+      {popularChannels && popularChannels.length > 0 ? (
+        <View contentContainerStyle={styles.channelList}>
+          {popularChannels.map((item) => (
+            <ChannelCard key={item.id} item={item} />
+          ))}
+        </View>
+      ) : (
+        <Text>Loading...</Text> // You can display a loading message here
+      )}
+    </ScrollView>
   );
 }
