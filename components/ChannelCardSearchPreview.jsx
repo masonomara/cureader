f911f0641efbf2f74a7a312169907a925a790348
masonomara <mason.omara@gmail.com>
2023-12-02 18:45:58 -0500

    init channelCardSearchPreview

---
+++ components/ChannelCardSearchPreview.jsx
@@ -0,0 +1,401 @@
+import { useState, useEffect, useLayoutEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  Pressable,
+  ActivityIndicator, // Import ActivityIndicator
+} from "react-native";
+import { useColorScheme } from "react-native";
+import { router, useNavigation } from "expo-router";
+import { supabase } from "../config/initSupabase";
+import Colors from "../constants/Colors";
+
+const CARD_WIDTH = Dimensions.get("window").width - 32;
+
+const textColorArray = [
+  "#E75450", // Red (Main Color)
+  "#66C0A9", // Green
+  "#FADA65", // Yellow
+  "#7929B2", // Purple
+  "#FF8C69", // Salmon
+  "#00B3A9", // Teal
+  "#E6532D", // Orange
+  "#3CB8B2", // Teal
+  "#FF7B00", // Orange
+  "#1A9E95", // Teal
+  "#E64400", // Red
+  "#2DC82D", // Green
+  "#FFD3A3", // Pale
+  "#00EB8F", // Green
+  "#E76E3F", // Orange
+  "#00ADC4", // Blue
+  "#FF9400", // Orange
+  "#6D5DC8", // Purple
+  "#FF8C69", // Salmon
+  "#7AC3D4", // Blue
+  "#C7132D", // Pink
+  "#8FEB8D", // Green
+  "#E64400", // Red
+  "#8560C1", // Purple
+  "#FFC800", // Gold
+  "#6988EF", // Blue
+];
+const colorArray = [
+  "#FF6961", // Red (Main Color)
+  "#78D2B2", // Green
+  "#FAEA96", // Yellow
+  "#8A2BE2", // Purple
+  "#FFA07A", // Salmon
+  "#00CED1", // Teal
+  "#FF6347", // Orange
+  "#48D1CC", // Teal
+  "#FF8C00", // Orange
+  "#20B2AA", // Teal
+  "#FF4500", // Red
+  "#74D674", // Green
+  "#FFDAB9", // Pale
+  "#00FA9A", // Green
+  "#FF7F50", // Orange
+  "#00BFFF", // Blue
+  "#FFA500", // Orange
+  "#7B68EE", // Purple
+  "#FFA07A", // Salmon
+  "#87CEEB", // Blue
+  "#DC143C", // Pink
+  "#98FB98", // Green
+  "#FF4500", // Red
+  "#9370DB", // Purple
+  "#FFD700", // Gold
+  "#849BE9", // Blue
+];
+
+export default function ChannelCardSearchPreview({ item, user }) {
+  const [isSubscribed, setIsSubscribed] = useState(
+    false
+  );
+  const [isOptimisticSubscribed, setIsOptimisticSubscribed] = useState(
+    false
+  );
+  const [subscribeButtonLoading, setSubscribeButtonLoading] = useState(true);
+
+  const navigation = useNavigation();
+  const colorScheme = useColorScheme();
+
+  useLayoutEffect(() => {
+    // Update state when the subscribed prop changes
+    setIsSubscribed(false);
+    setIsOptimisticSubscribed(false);
+    setSubscribeButtonLoading(false);
+  });
+
+  const handleSubscribe = async () => {
+    setIsOptimisticSubscribed(!isOptimisticSubscribed);
+    try {
+      const { data: userProfileData, error: userProfileError } = await supabase
+        .from("profiles")
+        .select()
+        .eq("id", user.id);
+
+      if (userProfileError) {
+        console.log("Error fetching user profile data:", userProfileError);
+        return;
+      }
+
+      const channelSubscriptions =
+        userProfileData[0].channel_subscriptions || [];
+      const itemChannelId = item.id;
+      const isAlreadySubscribed = channelSubscriptions.some(
+        (subscription) => subscription.channelId === itemChannelId
+      );
+
+      if (isSubscribed && isAlreadySubscribed) {
+        // Unsubscribe
+        const updatedSubscriptions = channelSubscriptions.filter(
+          (subscription) => subscription.channelId !== itemChannelId
+        );
+        await updateSubscriptions(user.id, updatedSubscriptions);
+
+        const { data: channelData, error: channelError } = await supabase
+          .from("channels")
+          .select()
+          .eq("id", item.id);
+
+        if (!channelError) {
+          const channel = channelData[0];
+          const updatedSubscribers = channel.channel_subscribers.filter(
+            (subscriber) => subscriber !== user.id
+          );
+          await updateChannelSubscribers(item.id, updatedSubscribers);
+        }
+      } else {
+        // Subscribe
+        const newSubscription = {
+          channelId: item.id,
+          channelUrl: item.channel_url,
+        };
+        const updatedSubscriptions = [...channelSubscriptions, newSubscription];
+        await updateSubscriptions(user.id, updatedSubscriptions);
+
+        const { data: channelData, error: channelError } = await supabase
+          .from("channels")
+          .select()
+          .eq("id", item.id);
+
+        if (!channelError) {
+          const channel = channelData[0];
+          const updatedSubscribers = [...channel.channel_subscribers, user.id];
+          await updateChannelSubscribers(item.id, updatedSubscribers);
+        }
+      }
+
+      setIsSubscribed(!isSubscribed);
+    } catch (error) {
+      console.error("Error handling subscription:", error);
+      setIsOptimisticSubscribed(!isOptimisticSubscribed);
+    }
+  };
+
+  const updateSubscriptions = async (userId, updatedSubscriptions) => {
+    await supabase
+      .from("profiles")
+      .update({ channel_subscriptions: updatedSubscriptions })
+      .eq("id", userId);
+  };
+
+  const updateChannelSubscribers = async (channelId, updatedSubscribers) => {
+    await supabase.from("channels").upsert([
+      {
+        id: channelId,
+        channel_subscribers: updatedSubscribers,
+      },
+    ]);
+  };
+
+  // Functions to get background color based on the first letter
+  const getColorForLetter = (letter) => {
+    const index = letter.toUpperCase().charCodeAt(0) % colorArray.length;
+    return colorArray[index];
+  };
+  const getTextColorForLetter = (letter) => {
+    const index = letter.toUpperCase().charCodeAt(0) % textColorArray.length;
+    return textColorArray[index];
+  };
+
+  const styles = {
+    card: {
+      backgroundColor: `${Colors[colorScheme || "light"].background}`,
+      borderBottomWidth: 1,
+      borderColor: `${Colors[colorScheme || "light"].border}`,
+      alignItems: "center",
+      flexDirection: "row",
+      display: "flex",
+      flex: 1,
+      width: CARD_WIDTH,
+      gap: 0,
+      paddingVertical: 12,
+      height: 89,
+      minHeight: 89,
+      maxHeight: 89,
+    },
+    cardContent: {
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "row",
+      flex: 1,
+      paddingLeft: 12,
+      paddingRight: 0,
+      gap: 8,
+    },
+    cardInfo: {
+      flex: 1,
+      alignItems: "flex-start",
+      justifyContent: "flex-start",
+      overflow: "hidden",
+      height: 64,
+      marginTop: -2,
+      arginBottom: -2,
+    },
+    title: {
+      display: "flex",
+      flexDirection: "row",
+      width: "100%",
+      alignItems: "flex-start",
+      flexWrap: "wrap",
+      color: `${Colors[colorScheme || "light"].textHigh}`,
+      fontFamily: "InterSemiBold",
+      fontWeight: "600",
+      fontSize: 17,
+      lineHeight: 22,
+      letterSpacing: -0.17,
+      marginBottom: 2,
+    },
+    cardControls: {
+      flexDirection: "row",
+      gap: 12,
+      alignItems: "flex-end",
+    },
+    description: {
+      flex: 1,
+      maxHeight: 38,
+      color: `${Colors[colorScheme || "light"].textMedium}`,
+      fontFamily: "InterRegular",
+      fontWeight: "400",
+      fontSize: 14,
+      lineHeight: 19,
+      letterSpacing: -0.14,
+    },
+    subscribeButton: {
+      backgroundColor: `${Colors[colorScheme || "light"].colorPrimary}`,
+      borderRadius: 100,
+      width: 88,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: 34,
+    },
+    subscribedButton: {
+      backgroundColor: `${Colors[colorScheme || "light"].surfaceOne}`,
+      borderRadius: 100,
+      width: 88,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: 34,
+      opacity: 0.87,
+    },
+    subscribeButtonText: {
+      color: `${Colors[colorScheme || "light"].colorOn}`,
+      fontFamily: "InterBold",
+      fontWeight: "700",
+      fontSize: 15,
+      lineHeight: 20,
+      letterSpacing: -0.15,
+    },
+    subscribedButtonText: {
+      color: `${Colors[colorScheme || "light"].colorPrimary}`,
+      fontFamily: "InterSemiBold",
+      fontWeight: "600",
+      fontSize: 15,
+      lineHeight: 20,
+      letterSpacing: -0.15,
+    },
+    noImageContainer: {
+      height: 64,
+      width: 64,
+      borderRadius: 10,
+      backgroundColor: getColorForLetter(item.channel_title[0]),
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      overflow: "hidden",
+    },
+    noImageContainerText: {
+      fontFamily: "NotoSerifMedium",
+      fontWeight: "500",
+      fontSize: 23,
+      lineHeight: 26,
+      letterSpacing: -0.173,
+      height: 26,
+      color: getTextColorForLetter(item.channel_title[0]),
+      textAlignVertical: "center",
+      textAlign: "center",
+      width: "1000%",
+    },
+  };
+
+  return (
+    <Pressable
+      style={styles.card}
+      onPress={() => {
+        router.push({
+          pathname: "/feedChannel",
+          params: {
+            title: item.channel_title,
+            description: item.channel_description,
+            image: item.channel_image_url,
+            subscribers: item.channel_subscribers,
+            url: item.channel_url,
+            id: item.id,
+            user: user,
+            userId: user.id,
+            subscribed: isSubscribed,
+          },
+        });
+      }}
+    >
+      {!item.channel_image_url ? (
+        <View style={styles.noImageContainer}>
+          <Text style={styles.noImageContainerText}>
+            {item.channel_title} {item.channel_title}
+          </Text>
+          <Text style={styles.noImageContainerText}>
+            {item.channel_title} {item.channel_title} {item.channel_title}
+          </Text>
+          <Text style={styles.noImageContainerText}>
+            {item.channel_title} {item.channel_title}
+          </Text>
+        </View>
+      ) : (
+        <View
+          style={{
+            aspectRatio: "1/1",
+            width: 64,
+            overflow: "hidden",
+            borderRadius: 10,
+          }}
+        >
+          <Image
+            style={{
+              flex: 1,
+              width: "100%",
+              height: "100%",
+            }}
+            source={{ uri: item.channel_image_url }}
+          />
+        </View>
+      )}
+      <View style={styles.cardContent}>
+        <View style={styles.cardInfo}>
+          <Text style={styles.title} numberOfLines={2}>
+            {item.channel_title}
+          </Text>
+          <Text style={styles.description} numberOfLines={2}>
+            {item.channel_description}
+          </Text>
+        </View>
+        <View style={styles.cardControls}>
+          <TouchableOpacity
+            style={
+              isOptimisticSubscribed
+                ? styles.subscribedButton
+                : styles.subscribeButton
+            }
+            onPress={handleSubscribe}
+          >
+            {subscribeButtonLoading === true ? (
+              <ActivityIndicator
+                size="small"
+                color={Colors[colorScheme || "light"].colorOn}
+              />
+            ) : (
+              <Text
+                style={
+                  isOptimisticSubscribed.toString() === "true"
+                    ? styles.subscribedButtonText
+                    : styles.subscribeButtonText
+                }
+              >
+                {isOptimisticSubscribed.toString() === "true"
+                  ? "Following"
+                  : "Follow"}
+              </Text>
+            )}
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Pressable>
+  );
+}
