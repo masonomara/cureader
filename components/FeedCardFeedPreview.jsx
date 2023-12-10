08aa9594d8ae32f60a6435582405de4764c00c06
masonomara <mason.omara@gmail.com>
2023-12-10 17:45:48 -0500

    removed console.logs

--- components/FeedCardFeedPreview.jsx
+++ components/FeedCardFeedPreview.jsx
@@ -82,9 +82,6 @@ export default function FeedCardFeedPreview({ item }) {
     setUserSubscriptionUrls,
   } = useContext(AuthContext);
 
-  console.log(itemId);
-  console.log(userSubscriptionIds);
-
   const [isSubscribed, setIsSubscribed] = useState(
     userSubscriptionIds.includes(itemId)
   );
