21b7ca383845e05ba095bd3f48ffed59d593d36e
masonomara <mason.omara@gmail.com>
2023-12-10 19:21:32 -0500

    got rid of comment

--- app/(home)/_layout.jsx
+++ app/(home)/_layout.jsx
@@ -3,9 +3,6 @@ import { Link, Tabs } from "expo-router";
 import { Pressable, useColorScheme } from "react-native";
 import Colors from "../../constants/Colors";
 
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
 function TabBarIcon(props) {
   return <Feather size={28} style={{ marginBottom: -3 }} {...props} />;
 }
