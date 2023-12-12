4d158d2c4c4fc0fa7d695a8127b8f1dbae087d3d
masonomara <mason.omara@gmail.com>
2023-12-12 17:30:12 -0500

    began formatting props page

--- app/(home)/bookmarks.jsx
+++ app/(home)/bookmarks.jsx
@@ -1,87 +1,82 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Alert, useColorScheme, TextInput } from "react-native";
+import React, { useContext } from "react";
+import { Alert, useColorScheme } from "react-native";
 import { FlashList } from "@shopify/flash-list";
-import { router } from "expo-router";
-import { supabase } from "../../config/initSupabase";
+import { AuthContext } from "../_layout";
 import { View } from "../../components/Themed";
-import * as rssParser from "react-native-rss-parser";
 import ArticleCard from "../../components/ArticleCard";
 import Colors from "../../constants/Colors";
-import { FeedContext } from "../_layout";
 
 export default function Bookmarks() {
-  const { feeds } = useContext(FeedContext);
+  const { user, userBookmarks } =
+    useContext(AuthContext);
+
   const colorScheme = useColorScheme();
-  const [user, setUser] = useState(null);
 
-  // redirect based on if user exists
-  useEffect(() => {
-    supabase.auth.getUser().then(({ data: { user } }) => {
-      if (user) {
-        setUser(user);
-      }
-    });
-  }, []);
-
-  // parse feeds
-  useEffect(() => {
-    const feedUrls = [
-      "https://feeds.megaphone.fm/newheights",
-      "http://www.nasa.gov/rss/dyn/breaking_news.rss",
-      "https://podcastfeeds.nbcnews.com/RPWEjhKq",
-      // Add more RSS feed URLs here
-    ];
-
-    const fetchAndParseFeeds = async () => {
-      const allChannels = [];
-      const allItems = [];
-
-      const parseAndSort = async (url) => {
-        try {
-          const response = await fetch(url);
-          const responseData = await response.text();
-          const parsedRss = await rssParser.parse(responseData);
-
-          allChannels.push({
-            title: parsedRss.title,
-            description: parsedRss.description,
-          });
-
-          allItems.push(
-            ...parsedRss.items.map((item) => ({
-              ...item,
-              publicationDate: new Date(item.published),
-              channel: parsedRss.title, // Include the channel title in the item
-              image: parsedRss.image,
-              channelUrl: parsedRss.links[0].url,
-            }))
-          );
-        } catch (error) {
-          console.error(error);
-        }
-      };
-
-      await Promise.all(feedUrls.map(parseAndSort));
-
-      // Sort items by publication date in descending order (most recent first)
-      allItems.sort((a, b) => b.publicationDate - a.publicationDate);
-
-      setRssChannels(allChannels);
-      setRssItems(allItems);
-    };
-
-    fetchAndParseFeeds();
-  }, []);
 
   const styles = {
+    noFeedsHeader: {
+      width: "100%",
+      alignItems: "center",
+      padding: 24,
+      paddingHorizontal: 8,
+      paddingBottom: 48,
+    },
+    username: {
+      marginBottom: 4,
+      marginTop: 4,
+      color: `${Colors[colorScheme || "light"].textHigh}`,
+      fontFamily: "NotoSerifMedium",
+      fontWeight: "500",
+      fontSize: 29,
+      lineHeight: 35,
+      letterSpacing: -0.217,
+    },
+    subtitle: {
+      marginBottom: 36,
+      color: `${Colors[colorScheme || "light"].textHigh}`,
+      fontFamily: "InterMedium",
+      fontWeight: "700",
+      fontSize: 19,
+      textAlign: "center",
+      lineHeight: 24,
+      letterSpacing: -0.19,
+    },
+    button: {
+      height: 48,
+      width: "100%",
+      flexDirection: "row",
+      backgroundColor: `${Colors[colorScheme || "light"].colorPrimary}`,
+      borderRadius: 18,
+      alignItems: "center",
+      justifyContent: "center",
+      paddingVertical: 8,
+    },
+    buttonText: {
+      color: `${Colors[colorScheme || "light"].colorOn}`,
+      fontFamily: "InterBold",
+      fontWeight: "700",
+      fontSize: 17,
+      lineHeight: 22,
+      letterSpacing: -0.17,
+    },
     container: {
       flex: 1,
       alignItems: "center",
-      justifyContent: "center",
+      justifyContent: "flex-start",
+      backgroundColor: `${Colors[colorScheme || "light"].background}`,
     },
     articleList: {
+      //backgroundColor: `${Colors[colorScheme || "light"].background}`,
       width: "100%",
       flex: 1,
+    },
+    feedList: {
+      width: "100%",
+      flex: 1,
+      paddingHorizontal: 16,
+    },
+    feedListFooter: {
+      height: 16,
     },
     input: {
       width: "100%",
@@ -111,42 +106,132 @@ export default function Bookmarks() {
       lineHeight: 22,
       letterSpacing: -0,
     },
+    buttonDisabled: {
+      height: 48,
+      width: "100%",
+      flexDirection: "row",
+      backgroundColor: `${Colors[colorScheme || "light"].buttonMuted}`,
+      borderRadius: 18,
+      alignItems: "center",
+      justifyContent: "center",
+      paddingVertical: 8,
+    },
+    buttonText: {
+      color: `${Colors[colorScheme || "light"].colorOn}`,
+      fontFamily: "InterBold",
+      fontWeight: "700",
+      fontSize: 17,
+      lineHeight: 22,
+      letterSpacing: -0.17,
+    },
+    headerWrapper: {
+      paddingHorizontal: 0,
+      paddingVertical: 12,
+      gap: 3,
+      width: "100%",
+      maxWidth: "100%",
+      height: 86,
+    },
+    titleWrapper: {
+      marginTop: 0,
+      width: "100%",
+      marginTop: 8,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    headerSubtitle: {
+      color: `${Colors[colorScheme || "light"].textLow}`,
+      fontFamily: "InterMedium",
+      fontWeight: "500",
+      fontSize: 15,
+      lineHeight: 20,
+      letterSpacing: -0.15,
+    },
+    title: {
+      color: `${Colors[colorScheme || "light"].textHigh}`,
+      fontFamily: "InterBold",
+      fontWeight: "700",
+      fontSize: 24,
+      lineHeight: 31,
+      letterSpacing: -0.24,
+    },
+    textButton: {
+      paddingHorizontal: 8,
+      paddingVertical: 3,
+    },
+    textButtonText: {
+      fontFamily: "InterSemiBold",
+      fontWeight: "600",
+      fontSize: 15,
+      lineHeight: 20,
+      letterSpacing: -0.15,
+      color: `${Colors[colorScheme || "light"].colorPrimary}`,
+    },
+    dailyQuoteContainer: {
+      padding: 16,
+      paddingVertical: 24,
+      paddingBottom: 12,
+      marginBottom: 0,
+      alignContent: "center",
+      justifyContent: "center",
+      flex: 1,
+      backgroundColor: `${Colors[colorScheme || "light"].background}`,
+    },
+    dailyQuoteContainerLoading: {
+      padding: 16,
+      paddingVertical: 24,
+      paddingBottom: 12,
+      marginBottom: 0,
+      alignContent: "center",
+      justifyContent: "center",
+      flex: 1,
+      backgroundColor: `${Colors[colorScheme || "light"].background}`,
+      position: "absolute",
+      zIndex: -1,
+    },
+    dailyQuoteWrapper: {
+      gap: 7,
+      alignContent: "center",
+      justifyContent: "center",
+    },
+    dailyQuoteQuote: {
+      textAlign: "center",
+      color: `${Colors[colorScheme || "light"].textHigh}`,
+      fontFamily: "NotoSerifMedium",
+      fontWeight: "500",
+      fontSize: 19,
+      lineHeight: 24,
+      letterSpacing: -0.1425,
+    },
+    dailyQuoteAuthor: {
+      textAlign: "center",
+      color: `${Colors[colorScheme || "light"].textMedium}`,
+      fontFamily: "InterMedium",
+      fontWeight: "500",
+      fontSize: 14,
+      lineHeight: 19,
+      letterSpacing: -0.14,
+    },
   };
 
   return (
     <View style={styles.container}>
-      {/* <View>
-        <Text numberOfLines={20}>{JSON.stringify(user, null, 2)}</Text>
-        <TouchableOpacity onPress={doLogout}>
-          <Text>Log out</Text>
-        </TouchableOpacity>
-      </View> */}
-      <TextInput
-        style={styles.input}
-        label="Email"
-        // onChangeText={(text) => setEmail(text)}
-        // value={email}
-        placeholder="email"
-        autoCapitalize={"none"}
-        autoCorrect={false}
-      />
       <View style={styles.articleList}>
         <FlashList
-          data={feeds}
+          data={userBookmarks}
           showsVerticalScrollIndicator={false}
           showsHorizontalScrollIndicator={false}
           estimatedItemSize={200}
-          renderItem={({ item }) => {
-            return (
-              <ArticleCard
-                item={item}
-                publication={item.channel}
-                image={item.image}
-                channelUrl={item.channelUrl}
-                user={user}
-              />
-            );
-          }}
+          renderItem={({ item }) => (
+            <ArticleCard
+              fallbackImage={item.fallbackImage}
+              item={item}
+              feed={item.feed}
+              publication={item.feed.channel_title}
+              user={user}
+            />
+          )}
         />
       </View>
     </View>
