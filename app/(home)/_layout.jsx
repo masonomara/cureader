74e9bb8994b6ce1937254c65bf022b74376d92bb
masonomara <mason.omara@gmail.com>
2023-12-12 03:16:06 -0500

    made soem common tab styles for conciseness

--- app/(home)/_layout.jsx
+++ app/(home)/_layout.jsx
@@ -1,10 +1,9 @@
 import Feather from "@expo/vector-icons/Feather";
 import { Tabs } from "expo-router";
-import { useColorScheme, View } from "react-native";
+import { useColorScheme, View, Alert } from "react-native";
 import { router } from "expo-router";
 import Colors from "../../constants/Colors";
 import { useContext } from "react";
-import { AuthContext } from "../_layout";
 import {
   Menu,
   MenuOptions,
@@ -20,7 +19,6 @@ function TabBarIcon(props) {
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-  const { user } = useContext(AuthContext);
 
   const showErrorAlert = (message) => {
     Alert.alert("Error", message);
@@ -32,6 +30,42 @@ export default function TabLayout() {
     if (error) {
       showErrorAlert("Error signing out: " + error.message);
     }
+  };
+
+  const commonScreenOptions = {
+    headerStyle: {
+      shadowColor: "transparent",
+      backgroundColor: Colors[colorScheme || "light"].background,
+    },
+    headerTitleStyle: {
+      color: Colors[colorScheme || "light"].textHigh,
+      fontFamily: "InterSemiBold",
+      fontWeight: "600",
+      fontSize: 17,
+      lineHeight: 22,
+      letterSpacing: -0.17,
+    },
+  };
+
+  const commonTabOptions = (name, title, icon) => ({
+    name,
+    options: {
+      title,
+      tabBarIcon: ({ color }) => <TabBarIcon name={icon} color={color} />,
+      ...commonScreenOptions,
+    },
+  });
+
+  const headerButtonStyle = {
+    marginRight: 16,
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    color: Colors[colorScheme || "light"].buttonActive,
+    fontFamily: "InterMedium",
+    fontWeight: "500",
+    fontSize: 15,
+    lineHeight: 20,
+    letterSpacing: -0.15,
   };
 
   const styles = {
@@ -84,61 +118,17 @@ export default function TabLayout() {
         tabBarInactiveTintColor: Colors[colorScheme || "light"].tabIconDefault,
       }}
     >
+      <Tabs.Screen {...commonTabOptions("index", "Feed", "home")} />
+      <Tabs.Screen {...commonTabOptions("explore", "Explore", "search")} />
       <Tabs.Screen
-        name="index"
-        options={{
-          headerStyle: {
-            shadowColor: "transparent", // Remove shadow on iOS
-            backgroundColor: Colors[colorScheme || "light"].background,
-          },
-          title: "Feed",
-          headerTitleStyle: styles.headerTitleText, // Set the style for the header title
-
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: "Explore",
-          headerStyle: {
-            shadowColor: "transparent", // Remove shadow on iOS
-            backgroundColor: Colors[colorScheme || "light"].background,
-          },
-          headerTitleStyle: styles.headerTitleText, // Set the style for the header title
-
-          tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="bookmarks"
-        options={{
-          title: "Bookmarks",
-          headerStyle: {
-            shadowColor: "transparent", // Remove shadow on iOS
-            backgroundColor: Colors[colorScheme || "light"].background,
-          },
-          headerTitleStyle: styles.headerTitleText, // Set the style for the header title
-
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="bookmark" color={color} />
-          ),
-        }}
+        {...commonTabOptions("bookmarks", "Bookmarks", "bookmark")}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profile",
-          headerStyle: {
-            shadowColor: "transparent", // Remove shadow on iOS
-            backgroundColor: Colors[colorScheme || "light"].background,
-          },
-          headerTitleStyle: styles.headerTitleText, // Set the style for the header title
+          ...commonScreenOptions,
           headerRight: () => (
-            // <TouchableOpacity style={styles.headerButton}>
-            //   <Text style={styles.headerButtonText}>Settings</Text>
-            // </TouchableOpacity>
-
             <Menu renderer={renderers.SlideInMenu}>
               <MenuTrigger
                 text="Settings"
@@ -146,28 +136,9 @@ export default function TabLayout() {
                   triggerTouchable: {
                     underlayColor: "transparent",
                     activeOpacity: 0.2,
-                    style: {
-                      marginRight: 16,
-                      flexDirection: "row",
-                      flexWrap: "nowrap",
-                      color: `${Colors[colorScheme || "light"].buttonActive}`,
-                      fontFamily: "InterMedium",
-                      fontWeight: "500",
-                      fontSize: 15,
-                      lineHeight: 20,
-                      letterSpacing: -0.15,
-                    },
+                    style: headerButtonStyle,
                   },
-                  triggerText: {
-                    flexDirection: "row",
-                    flexWrap: "nowrap",
-                    color: `${Colors[colorScheme || "light"].buttonActive}`,
-                    fontFamily: "InterMedium",
-                    fontWeight: "500",
-                    fontSize: 15,
-                    lineHeight: 20,
-                    letterSpacing: -0.15,
-                  },
+                  triggerText: headerButtonStyle,
                 }}
               />
               <MenuOptions
@@ -195,34 +166,16 @@ export default function TabLayout() {
                     justifyContent: "center",
                     paddingHorizontal: 0,
                     height: 44,
-                    // borderWidth: 1,
-                    // borderColor: "red",
                   },
                   optionTouchable: {
                     underlayColor: "transparent",
                     activeOpacity: 0.2,
                   },
-                  optionText: {
-                    color: Colors[colorScheme || "light"].buttonActive,
-                    fontFamily: "InterMedium",
-                    fontWeight: "500",
-                    fontSize: 15,
-                    lineHeight: 20,
-                    letterSpacing: -0.15,
-                  },
+                  optionText: headerButtonStyle,
                 }}
               >
-                <MenuOption
-                  onSelect={() => {
-                    doLogout();
-                  }}
-                  text="Log Out"
-                />
-                <View style={styles.tooltipDivider}></View>
-                {/* <MenuOption
-                  
-                  text="Turn off Daily Quote"
-                /> */}
+                <MenuOption onSelect={doLogout} text="Log Out" />
+                <View style={commonScreenOptions.tooltipDivider}></View>
               </MenuOptions>
             </Menu>
           ),
