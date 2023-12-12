0ab8c37a091e5a1f21458098f0a3df6f1eb06a62
masonomara <mason.omara@gmail.com>
2023-12-12 02:22:16 -0500

    index working well

--- app/(home)/index.jsx
+++ app/(home)/index.jsx
@@ -24,95 +24,135 @@ export default function Index() {
     Alert.alert("Error", message);
   };
 
-  const fetchAndParseFeeds = async (refresh = false) => {
-    try {
-      if (feeds && userSubscriptionUrls) {
-        const fallbackImages = feeds.map((feed) => ({
-          channel_url: feed.channel_url,
-          channel_image_url: feed.channel_image_url,
-        }));
+  const fetchAndParseFeeds = async (urls) => {
+    const fallbackImages = feeds.map((feed) => ({
+      channel_url: feed.channel_url,
+      channel_image_url: feed.channel_image_url,
+    }));
 
-        const allChannels = [];
-        const allItems = [];
+    const allChannels = [];
+    const allItems = [];
 
-        const parseAndSort = async (url) => {
-          try {
-            const response = await fetch(url);
+    const parseAndSort = async (url) => {
+      try {
+        const response = await fetch(url);
 
-            if (!response.ok) {
-              throw new Error(
-                `Network response not OK. Status: ${response.status}`
-              );
-            }
+        if (!response.ok) {
+          throw new Error(
+            `Network response not OK. Status: ${response.status}`
+          );
+        }
 
-            const responseData = await response.text();
-            const parsedRss = await rssParser.parse(responseData);
+        const responseData = await response.text();
+        const parsedRss = await rssParser.parse(responseData);
 
-            const channelImage = fallbackImages.find(
-              (image) => image.channel_url === url
-            );
-            const feed = feeds.find((feed) => feed.channel_url === url);
+        const channelImage = fallbackImages.find(
+          (image) => image.channel_url === url
+        );
+        const feed = feeds.find((feed) => feed.channel_url === url);
 
-            allChannels.push({
-              title: parsedRss.title,
-              description: parsedRss.description,
-            });
+        allChannels.push({
+          title: parsedRss.title,
+          description: parsedRss.description,
+        });
 
-            allItems.push(
-              ...parsedRss.items.map((item) => ({
-                ...item,
-                publicationDate: new Date(item.published),
-                feed: feed,
-                image: parsedRss.image,
-                fallbackImage: channelImage
-                  ? channelImage.channel_image_url
-                  : null,
-                channelUrl: parsedRss.links[0].url,
-              }))
-            );
-          } catch (error) {
-            console.error(`Error parsing URL: ${url}`, error);
-            // Log or display more information about the error if needed
-            showErrorAlert(
-              `Error fetching RSS feed from ${url}. Please try again.`
-            );
-          }
-        };
-
-        await Promise.all(userSubscriptionUrls.map(parseAndSort));
-
-        // Sort items by publication date in descending order (most recent first)
-        allItems.sort((a, b) => b.publicationDate - a.publicationDate);
-
-        setRssChannels(allChannels);
-        setRssItems(allItems);
-        setFeedsParsed(true);
+        allItems.push(
+          ...parsedRss.items.map((item) => ({
+            ...item,
+            publicationDate: new Date(item.published),
+            feed: feed,
+            image: parsedRss.image,
+            fallbackImage: channelImage ? channelImage.channel_image_url : null,
+            channelUrl: parsedRss.links[0].url,
+          }))
+        );
+      } catch (error) {
+        console.error(`Error parsing URL: ${url}`, error);
+        showErrorAlert(
+          `Error fetching RSS feed from ${url}. Please try again.`
+        );
       }
-    } catch (error) {
-      console.error(
-        `Error ${refresh ? "refreshing" : ""} and parsing feeds:`,
-        error
-      );
-      // Log or display more information about the error if needed
-      showErrorAlert(
-        `Error ${
-          refresh ? "refreshing" : "fetching and parsing"
-        } RSS feeds. Please try again.`
-      );
-    } finally {
-      setIsRefreshing(false);
+    };
+
+    await Promise.all(urls.map(parseAndSort));
+
+    allItems.sort((a, b) => b.publicationDate - a.publicationDate);
+
+    setRssItems(allItems);
+    setFeedsParsed(true);
+  };
+
+  useEffect(() => {
+    if (user && feeds && userSubscriptionUrls) {
+      fetchAndParseFeeds(userSubscriptionUrls).finally(() => {
+        setIsRefreshing(false);
+      });
+    }
+  }, [user, feeds, userSubscriptionUrls]);
+
+  const fetchAndParseFeedsRefresh = async () => {
+    if (user && feeds && userSubscriptionUrls) {
+      const fallbackImages = feeds.map((feed) => ({
+        channel_url: feed.channel_url,
+        channel_image_url: feed.channel_image_url,
+      }));
+
+      const allChannels = [];
+      const allItems = [];
+
+      const parseAndSort = async (url) => {
+        try {
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const responseData = await response.text();
+          const parsedRss = await rssParser.parse(responseData);
+
+          const channelImage = fallbackImages.find(
+            (image) => image.channel_url === url
+          );
+          const feed = feeds.find((feed) => feed.channel_url === url);
+
+          allChannels.push({
+            title: parsedRss.title,
+            description: parsedRss.description,
+          });
+
+          allItems.push(
+            ...parsedRss.items.map((item) => ({
+              ...item,
+              publicationDate: new Date(item.published),
+              feed: feed,
+              image: parsedRss.image,
+              fallbackImage: channelImage
+                ? channelImage.channel_image_url
+                : null,
+              channelUrl: parsedRss.links[0].url,
+            }))
+          );
+        } catch (error) {
+          console.error(error);
+          showErrorAlert("Error refreshing RSS feeds. Please try again.");
+        }
+      };
+
+      await Promise.all(userSubscriptionUrls.map(parseAndSort));
+
+      allItems.sort((a, b) => b.publicationDate - a.publicationDate);
+
+      setRssChannels(allChannels);
+      setRssItems(allItems);
+      setFeedsParsed(true);
     }
   };
 
   const onRefresh = () => {
-    // set isRefreshing to true
     setIsRefreshing(true);
-    fetchAndParseFeeds(true);
+    fetchAndParseFeedsRefresh().finally(() => {
+      setIsRefreshing(false);
+    });
   };
-
-  useEffect(() => {
-    fetchAndParseFeeds();
-  }, [user, feeds, userSubscriptionUrls]);
 
   // Styles
   const styles = {
@@ -319,44 +359,108 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-      {feedsParsed && (
-        <View style={styles.articleList}>
-          <FlashList
-            ListHeaderComponent={() => (
-              <View style={styles.dailyQuoteContainer}>
-                {dailyQuote && dailyQuote.length > 0 && (
-                  <View style={styles.dailyQuoteWrapper}>
-                    <Text style={styles.dailyQuoteQuote}>
-                      “{dailyQuote[0].q}”
+      <View style={styles.dailyQuoteContainerLoading}>
+        {dailyQuote && dailyQuote.length > 0 && (
+          <View style={styles.dailyQuoteWrapper}>
+            <Text style={styles.dailyQuoteQuote}>“{dailyQuote[0].q}”</Text>
+            <Text style={styles.dailyQuoteAuthor}>- {dailyQuote[0].a}</Text>
+          </View>
+        )}
+      </View>
+      {feedsParsed ? (
+        rssItems.length > 0 ? (
+          <View style={styles.articleList}>
+            <FlashList
+              ListHeaderComponent={() => (
+                <View style={styles.dailyQuoteContainer}>
+                  {dailyQuote && dailyQuote.length > 0 && rssChannels > 0 && (
+                    <View style={styles.dailyQuoteWrapper}>
+                      <Text style={styles.dailyQuoteQuote}>
+                        “{dailyQuote[0].q}”
+                      </Text>
+                      <Text style={styles.dailyQuoteAuthor}>
+                        - {dailyQuote[0].a}
+                      </Text>
+                    </View>
+                  )}
+                </View>
+              )}
+              data={rssItems}
+              showsVerticalScrollIndicator={false}
+              showsHorizontalScrollIndicator={false}
+              refreshing={isRefreshing}
+              onRefresh={onRefresh}
+              estimatedItemSize={200}
+              renderItem={({ item }) => (
+                <ArticleCard
+                  fallbackImage={item.fallbackImage}
+                  feeds={feeds}
+                  item={item}
+                  feed={item.feed}
+                  publication={item.feed.channel_title}
+                  user={user}
+                  userSubscriptionIds={userSubscriptionIds}
+                  userSubscriptionUrls={userSubscriptionUrls}
+                />
+              )}
+            />
+          </View>
+        ) : (
+          <View style={styles.feedList}>
+            <FlashList
+              ListHeaderComponent={() => (
+                <>
+                  <View style={styles.noFeedsHeader}>
+                    <Text style={styles.username}>Welcome to Cureader!</Text>
+                    <Text style={styles.subtitle}>
+                      Feed free to view your Explore Page or see our most
+                      popular feeds below.
                     </Text>
-                    <Text style={styles.dailyQuoteAuthor}>
-                      - {dailyQuote[0].a}
+                    <TouchableOpacity
+                      style={styles.button}
+                      onPress={() => {
+                        router.push({
+                          pathname: "/explore",
+                        });
+                      }}
+                    >
+                      <Text style={styles.buttonText}>View Explore Page</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.headerWrapper}>
+                    <View style={styles.titleWrapper}>
+                      <Text style={styles.title}>Popular Feeds</Text>
+                      <TouchableOpacity
+                        style={styles.textButton}
+                        onPress={() => {
+                          router.push({
+                            pathname: "/allPopularFeeds",
+                          });
+                        }}
+                      >
+                        <Text style={styles.textButtonText}>View more</Text>
+                      </TouchableOpacity>
+                    </View>
+                    <Text style={styles.headerSubtitle}>
+                      Get started with our most popular feeds.
                     </Text>
                   </View>
-                )}
-              </View>
-            )}
-            data={rssItems}
-            showsVerticalScrollIndicator={false}
-            showsHorizontalScrollIndicator={false}
-            refreshing={isRefreshing}
-            onRefresh={onRefresh}
-            estimatedItemSize={200}
-            renderItem={({ item }) => (
-              <ArticleCard
-                fallbackImage={item.fallbackImage}
-                feeds={feeds}
-                item={item}
-                feed={item.feed}
-                publication={item.feed.channel_title}
-                user={user}
-                userSubscriptionIds={userSubscriptionIds}
-                userSubscriptionUrls={userSubscriptionUrls}
-              />
-            )}
-          />
-        </View>
+                </>
+              )}
+              data={popularFeeds}
+              showsVerticalScrollIndicator={false}
+              showsHorizontalScrollIndicator={false}
+              estimatedItemSize={200}
+              renderItem={({ item }) => (
+                <FeedCard key={item.id} item={item} user={user} />
+              )}
+              ListFooterComponent={() => <View style={styles.feedListFooter} />}
+            />
+          </View>
+        )
+      ) : (
+        <></>
       )}
     </View>
   );
-}
+};
