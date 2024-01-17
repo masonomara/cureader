2e9dc88815f5d83d0cc451f0caf7350b601c3143
masonomara <mason.omara@gmail.com>
2024-01-17 16:06:38 -0500

    improved supabase login

--- config/supabase.js
+++ config/supabase.js
@@ -1,25 +1,27 @@
 import "react-native-url-polyfill/auto";
 import { createClient } from "@supabase/supabase-js";
 import * as SecureStore from "expo-secure-store";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const ExpoSecureStoreAdapter = {
-  getItem: (key) => {
-    return SecureStore.getItemAsync(key);
-  },
-  setItem: (key, value) => {
-    SecureStore.setItemAsync(key, value);
-  },
-  removeItem: (key) => {
-    SecureStore.deleteItemAsync(key);
-  },
-};
+// const ExpoSecureStoreAdapter = {
+//   getItem: (key) => {
+//     return SecureStore.getItemAsync(key);
+//   },
+//   setItem: (key, value) => {
+//     SecureStore.setItemAsync(key, value);
+//   },
+//   removeItem: (key) => {
+//     SecureStore.deleteItemAsync(key);
+//   },
+// };
 
 export const supabase = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_URL,
   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
   {
     auth: {
-      storage: ExpoSecureStoreAdapter,
+      // storage: ExpoSecureStoreAdapter,
+      storage: AsyncStorage,
       autoRefreshToken: true,
       persistSession: true,
       detectSessionInUrl: false,
@@ -31,9 +33,10 @@ export const supabaseAuth = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_SERVICE_ROLE,
   {
     auth: {
-      storage: ExpoSecureStoreAdapter,
-      autoRefreshToken: false,
-      persistSession: false,
+      // storage: ExpoSecureStoreAdapter,
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
       detectSessionInUrl: false,
     },
   }
